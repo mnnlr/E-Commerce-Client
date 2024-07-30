7cc5d053cee2746d26f3bb35e873349b5774614f
rahulsingh <rahulpharthyal04@gmail.com>
2024-07-30 16:36:01 +0530

    add new backend url

--- src/CustomAxios/defaultAxios.js
+++ src/CustomAxios/defaultAxios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const defaultAxios = axios.create({
     // baseURL: 'http://localhost:3002',
-    baseURL: 'https://diwali-e-commerce-backend-n2a2.onrender.com',
+    baseURL: 'https://diwali-e-commerce-backend.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
